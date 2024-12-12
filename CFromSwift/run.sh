14bf7b82dce907170414f09588249893231430e1
Ali AlSalman <github@alimark71.com>
2024-12-12 21:04:05 +0300

    refine run script

--- CFromSwift/run.sh
+++ CFromSwift/run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$0")
+INITAL_DIR=$(pwd)
+
+cd $SCRIPT_DIR
+
 set +e 
 mkdir artifacts > /dev/null 2>&1
 set -e
 
 gcc -c expose.c -o artifacts/expose.o
-ar rcs artifacts/expose.a artifacts/expose.o
-swiftc -parse-as-library -enable-experimental-feature Extern into.swift artifacts/expose.a -o artifacts/into
+swiftc -parse-as-library -enable-experimental-feature Extern into.swift artifacts/expose.o -o artifacts/into
 
 ./artifacts/into
+
+cd $INITAL_DIR
--- SwiftFromC/run.sh
+++ SwiftFromC/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$0")
+INITAL_DIR=$(pwd)
+
+cd $SCRIPT_DIR
+
 set +e
 mkdir artifacts  > /dev/null 2>&1
 set -e
@@ -7,3 +12,5 @@ set -e
 swiftc -emit-library -static -o artifacts/expose.a expose.swift
 gcc into.c artifacts/expose.a -o artifacts/into
 ./artifacts/into
+
+cd $INITAL_DIR
